fbc6a99862e898118926e1cd3825294f9832a02b
tintinkurtti <tintinkurtti@hotmail.com>
2023-09-17 11:50:14 +0200

    insert funkar inte :(

--- int_sorted.cpp
+++ int_sorted.cpp
@@ -6,9 +6,17 @@
 #include "int_sorted.h"
 #include "int_buffer.h"
 
-
+int_sorted sort(const int* begin, const int* end){
+    if (begin == end){return int_sorted(nullptr, 0);}
+    if (begin == end -1) {return int_sorted(begin, 1);}
+    ptrdiff_t half = (end-begin)/2; //pointer diff type
+    const int* mid = begin + half;
+    return sort(begin, mid).merge(sort(mid, end));
+}
 
 int_sorted::int_sorted ( const int * source , size_t size ) : buf(size){
+    if(size <= 1){return;}
+    *this = sort(begin(),end());
 }
 
 size_t int_sorted::size () const {
@@ -16,19 +24,9 @@ size_t int_sorted::size () const {
 }
 
 void int_sorted::insert ( int value ) {
-    // Använd lower_bound för att hitta rätt plats att infoga värdet
-    auto position = std::lower_bound(begin(), end(), value);
 
-    // Infoga värdet vid den hittade positionen och öka bufferns storlek
-    buf = int_buffer(buf.size() + 1);
-
-    // Flytta alla element efter infogningspositionen ett steg åt höger
-    for (auto it = end() - 1; it != position; --it) {
-        *(it + 1) = *it;
-    }
-
-    // Sätt det nya värdet på infogningspositionen
-    *position = value;
+    int_sorted new_val(&value, 1);
+    buf = merge(new_val).buf;
 
 }
 
@@ -41,31 +39,35 @@ const int* int_sorted::end () const {
 }
 
 int_sorted int_sorted::merge ( const int_sorted &merge_with ) const {
+
     const int* this_ptr = begin();
     const int* merge_ptr = merge_with.begin();
-    int_sorted result;
+    int_buffer result(size() + merge_with.size());
 
+    int index = 0;
     // Slå samman buffrarna i rätt ordning
     while (this_ptr != end() && merge_ptr != merge_with.end()) {
+
         if (*this_ptr < *merge_ptr) {
-            result.insert(*this_ptr);
-            ++this_ptr;
+            result[index++] = *this_ptr++;
         } else {
-            result.insert(*merge_ptr);
-            ++merge_ptr;
+            result[index++] = *merge_ptr++;
         }
     }
 
     // Lägg till kvarvarande element från båda buffrarna (om det finns några)
     while (this_ptr != end()) {
-        result.insert(*this_ptr);
-        ++this_ptr;
+        result[index++] = *this_ptr++;
     }
 
     while (merge_ptr != merge_with.end()) {
-        result.insert(*merge_ptr);
-        ++merge_ptr;
+        result[index++] = *merge_ptr++;
     }
 
-    return result;
+    int_sorted finish(nullptr, 0);
+    finish.buf = result;
+
+    return finish;
 }
+
+
--- main.cpp
+++ main.cpp
@@ -21,9 +21,29 @@ int main() {
 
 
 
-    int_buffer b(10);
 
-    f(b);
+    // int_buffer b(10);
+
+    int_sorted a(nullptr, 0);
+
+    int_sorted b(nullptr, 0);
+
+    a.insert(5);
+    a.insert(2);
+    a.insert(1);
+
+    b.insert(8);
+    b.insert(3);
+    b.insert(11);
+
+
+    int_sorted c = b.merge(a);
+
+    for (const int* i = c.begin(); i != c.end(); i++){
+        std::cout << *i << ", ";
+    }
+
 
     return 0;
+
 }
